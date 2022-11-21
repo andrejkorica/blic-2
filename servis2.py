34ac4f37d09e1bf0a87b0d374da1fa6ed65e27de
Andrej <koricaandrej3@gmail.com>
2022-11-21 16:45:30 +0100

    2 i 3

---
+++ servis2.py
@@ -0,0 +1,36 @@
+import aiohttp
+import asyncio
+import time
+import aiosqlite
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.post("/filterUser")
+async def filterUser(request):
+        req = await request.json()
+        priv = req
+    try:
+        priv = priv.get("results")
+        priv = priv[0].get("name")
+
+        tasks.append({"ime": priv.get("first"),"prezime":priv.get("last")})
+        priv = req.get("results")
+
+        tasks.append({"city": priv[0].get("city"),"email":priv[0].get("email")})
+        
+        priv = []
+
+        print(tasks)
+        return web.json_response({"Status": "ok","message":req}, status=200)
+    except Exception as e:
+        return web.json_response({"Status": "error"}, status=500)
+
+
+app = web.Application()
+
+app.router.add_routes(routes)
+
+web.run_app(app, port=8081)
+
+        
---
+++ servis3.py
@@ -0,0 +1,27 @@
+import aiohttp
+import asyncio
+import time
+import aiosqlite
+
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.post("/filterJoke")
+async def filterJoke(request):
+    req = await request.json()
+    priv = []
+    try:
+        priv.append({"setup":req.get("setup")})
+        priv.append({"punchline":req.get("punchline")})
+        print(temp)
+        return web.json_response({"Status": "ok","jokez":req}, status=200)
+    except Exception as e:
+        return web.json_response({"Status": "error"}, status=500)
+
+
+app.router.add_routes(routes)
+
+web.run_app(app, port=8082)
+
